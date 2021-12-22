488eeda4b1e2e42b6af2f19e2994eee430c91593
Steve Larsen <slarsen-3@att.net>
2021-12-22 16:15:44 -0600

    changed res.setHeader

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ const cors = require('cors');
 // middleware
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader(
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Credentials', 'true');
+  response.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT'
+  );
+  response.setHeader(
     'Access-Control-Allow-Headers',
-    'Content-Type, Access-Control-Allow-Headers'
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
   );
   next();
 });
 app.use(express.json()); // req.body
-
 
 app.get('/', function (req, res) {
   //when we get an http get request to the root/homepage
@@ -34,7 +38,6 @@ app.use('/', require('./routes/login'));
 
 // exercise
 app.use('/', require('./routes/exercise'));
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
