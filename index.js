6b073ad768c27872a723eb869ea6ecdfdbe081b3
Steve Larsen <slarsen-3@att.net>
2021-12-22 16:16:16 -0600

    changed res.setHeader

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const cors = require('cors');
 app.use(express.json());
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Credentials', 'true');
-  response.setHeader(
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
     'Access-Control-Allow-Methods',
     'GET,HEAD,OPTIONS,POST,PUT'
   );
-  response.setHeader(
+  res.setHeader(
     'Access-Control-Allow-Headers',
     'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
   );
