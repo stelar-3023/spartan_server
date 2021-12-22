40a3b0688246f7e4be535432388ddf020e77ae01
Steve Larsen <slarsen-3@att.net>
2021-12-22 15:42:25 -0600

    changed routes

--- index.js
+++ index.js
@@ -7,44 +7,40 @@ const cors = require('cors');
 // process.env.PORT
 // process.env.NODE_ENV => production or development
 
-
-
-
 // middleware
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Access-Control-Allow-Headers"
+    'Access-Control-Allow-Headers',
+    'Content-Type, Access-Control-Allow-Headers'
   );
   next();
 });
 app.use(express.json()); // req.body
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // serve static files
   app.use(express.static('client/build'));
 }
 
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   //when we get an http get request to the root/homepage
-  res.send("Hello World");
+  res.send('Hello World');
 });
 
-
 // registration
-app.post('/', require('./routes/registration'));
+app.use('/registration', require('./routes/registration'));
 
 // login and verify
-app.post('/', require('./routes/login'));
+app.use('/login', require('./routes/login'));
 
 // exercise
-app.get('/', require('./routes/exercise'));
+app.use('/exercise', require('./routes/exercise'));
 
 // app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html' )); 
+//   res.sendFile(path.join(__dirname, '../client/build/index.html' ));
 // })
 
 app.listen(PORT, () => {
