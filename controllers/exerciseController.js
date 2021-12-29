5fa7e4e08f9777d1a10f4b9f1d12fc6cbe6e58d2
Steve Larsen <slarsen-3@att.net>
2021-12-28 18:18:45 -0600

    added controllers & moved logic from routes to controller

---
+++ controllers/exerciseController.js
@@ -0,0 +1,76 @@
+const pool = require('../db');
+//get all exercises
+const exercise_index = async (req, res) => {
+  try {
+    const email = req.params.email;
+    const exercises = await pool.query(
+      `SELECT * FROM exercises where user_email='${email}'`
+    );
+    res.json(exercises.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+// get exercise by id
+const exercise_details = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const exercise = await pool.query(
+      'SELECT * FROM exercises WHERE exercise_id = $1',
+      [id]
+    );
+    res.json(exercise.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+// update exercise
+const exercise_update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { exercise, reps, weight } = req.body;
+    console.log(req.body);
+
+    const updateExercise = await pool.query(
+      'UPDATE exercises SET exercise = $1, reps = $2, weight = $3 WHERE exercise_id = $4',
+      [exercise, reps, weight, id],
+      (err, result) => {
+        if (err) {
+          console.error(err.message);
+          res.status(500).send('Server Error');
+        } else {
+          res.status(200).send(req.body);
+        }
+      }
+    );
+    // res.json('Exercise updated');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+const exercise_delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteExercise = await pool.query(
+      'DELETE FROM exercises WHERE exercise_id = $1',
+      [id]
+    );
+    res.json('Exercise deleted');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+module.exports = {
+  exercise_index,
+  exercise_update,
+  exercise_details,
+  exercise_delete,
+};
--- routes/exercise.js
+++ routes/exercise.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
+const exerciseController = require('../controllers/exerciseController');
 
 // add exercise
 router.post('/exercises/:email', async (req, res) => {
@@ -19,73 +20,77 @@ router.post('/exercises/:email', async (req, res) => {
 });
 
 // get all exercises
-router.get('/exercises/:email', async (req, res) => {
-  try {
-    const email = req.params.email;
-    const exercises = await pool.query(
-      `SELECT * FROM exercises where user_email='${email}'`
-    );
-    res.json(exercises.rows);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+router.get('/exercises/:email', exerciseController.exercise_index);
+// router.get('/exercises/:email', async (req, res) => {
+//   try {
+//     const email = req.params.email;
+//     const exercises = await pool.query(
+//       `SELECT * FROM exercises where user_email='${email}'`
+//     );
+//     res.json(exercises.rows);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
 
 // get exercise by id
-router.get('/exercises/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const exercise = await pool.query(
-      'SELECT * FROM exercises WHERE exercise_id = $1',
-      [id]
-    );
-    res.json(exercise.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+router.get('/exercises/:id', exerciseController.exercise_details);
+// router.get('/exercises/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const exercise = await pool.query(
+//       'SELECT * FROM exercises WHERE exercise_id = $1',
+//       [id]
+//     );
+//     res.json(exercise.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
 
 // update exercise
-router.put('/exercises/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { exercise, reps, weight } = req.body;
-    console.log(req.body);
+router.put('/exercises/:id', exerciseController.exercise_update)
+// router.put('/exercises/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { exercise, reps, weight } = req.body;
+//     console.log(req.body);
 
-    const updateExercise = await pool.query(
-      'UPDATE exercises SET exercise = $1, reps = $2, weight = $3 WHERE exercise_id = $4',
-      [exercise, reps, weight, id],
-      (err, result) => {
-        if (err) {
-          console.error(err.message);
-          res.status(500).send('Server Error');
-        } else {
-          res.status(200).send(req.body);
-        }
-      }
-    );
-    // res.json('Exercise updated');
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+//     const updateExercise = await pool.query(
+//       'UPDATE exercises SET exercise = $1, reps = $2, weight = $3 WHERE exercise_id = $4',
+//       [exercise, reps, weight, id],
+//       (err, result) => {
+//         if (err) {
+//           console.error(err.message);
+//           res.status(500).send('Server Error');
+//         } else {
+//           res.status(200).send(req.body);
+//         }
+//       }
+//     );
+//     // res.json('Exercise updated');
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
 
 // delete exercise
-router.delete('/exercises/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deleteExercise = await pool.query(
-      'DELETE FROM exercises WHERE exercise_id = $1',
-      [id]
-    );
-    res.json('Exercise deleted');
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+router.delete('exercise/:id', exerciseController.exercise_delete);
+// router.delete('/exercises/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const deleteExercise = await pool.query(
+//       'DELETE FROM exercises WHERE exercise_id = $1',
+//       [id]
+//     );
+//     res.json('Exercise deleted');
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
 
 module.exports = router;
